4e8212613417bf3daa7402592fe6ea102cc46e0f
Florian <79457058+florian-alb@users.noreply.github.com>
2025-06-06 14:05:45 +0200

    ✅ ajout d'une sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,25 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { redirect } from "next/navigation";
+
+import { auth } from "@/app/auth";
 import "./globals.css";
+
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/sidebar/app-sidebar";
+import {
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbSeparator,
+  BreadcrumbPage,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,17 +36,52 @@ export const metadata: Metadata = {
   description: "Juse la meilleure application de sondage..",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
+
+  if (!session?.user) redirect("sign-in");
+
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <SidebarProvider>
+          <AppSidebar user={session.user} />
+          <SidebarInset>
+            <header className="flex h-16 shrink-0 items-center gap-2">
+              <div className="flex items-center gap-2 px-4">
+                <SidebarTrigger className="-ml-1" />
+                <Separator
+                  orientation="vertical"
+                  className="mr-2 data-[orientation=vertical]:h-4"
+                />
+                <Breadcrumb>
+                  <BreadcrumbList>
+                    <BreadcrumbItem className="hidden md:block">
+                      <BreadcrumbLink href="#">
+                        Building Your Application
+                      </BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator className="hidden md:block" />
+                    <BreadcrumbItem>
+                      <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                    </BreadcrumbItem>
+                  </BreadcrumbList>
+                </Breadcrumb>
+              </div>
+            </header>
+            <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+              <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min">
+                {children}
+              </div>
+            </div>
+          </SidebarInset>
+        </SidebarProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,3 @@
-import { LogoutButton } from "@/components/auth-buttons";
-import { auth } from "@/app/auth";
-import { GithubSignIn } from "@/components/github-sign-in";
-
-import { redirect } from "next/navigation";
-
-export default async function Home() {
-  const session = await auth();
-
-  if (!session) redirect("/sign-in");
-
-  return <div>{!session?.user ? <GithubSignIn /> : <LogoutButton />}</div>;
+export default function Page() {
+  return <div>salut </div>;
 }
